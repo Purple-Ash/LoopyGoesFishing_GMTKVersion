8c3cbcceefd4dbebdf08ed66994637544a74a1a4
skay44 <michal.1805@wp.pl>
2025-08-02 21:13:09 +0200

    added music

---
+++ Assets/Code/Scripts/AudioManager.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public enum Audio
+    {
+        Music,
+        SFX,
+        Ambient
+    }
+
+    [Header("Audio Sources")]
+    [SerializeField] private float _musicVolume = 1f;
+    [SerializeField] private float _sfxVolume = 1f;
+    [SerializeField] private float _ambientVolume = 1f;
+
+    [Header("Audio Clip")]
+    [SerializeField] private AudioClip _mainMusic;
+
+    private AudioSource _musicSource;
+    private List<AudioSource> _sfxSources;
+    private AudioSource _ambientSource;
+
+    private float GetVolume(Audio volumeSetting)
+    {
+        switch (volumeSetting)
+        {
+            case Audio.Music: return _musicVolume;
+            case Audio.SFX: return _sfxVolume;
+            case Audio.Ambient: return _ambientVolume;
+        }
+        Debug.LogWarning("Unknown Volume Enum!");
+        return _sfxVolume;
+    }
+
+    private void UpdateSFXVolume()
+    {
+        _sfxSources.RemoveAll(src => src == null);
+        foreach (var source in _sfxSources)
+        {
+            source.volume = _sfxVolume;
+        }
+    }
+
+    private void UpdateAmbientVolume()
+    {
+       _ambientSource.volume = _ambientVolume;
+    }
+
+    private void UpdateMusicVolume()
+    {
+        _musicSource.volume = _musicVolume;
+    }
+
+    public void SetVolume(Audio volumeSetting, float newVolume)
+    {
+        switch (volumeSetting)
+        {
+            case Audio.Music:
+                _musicVolume = newVolume;
+                UpdateMusicVolume();
+                return;
+            case Audio.SFX:
+                _sfxVolume = newVolume;
+                UpdateSFXVolume();
+                return;
+            case Audio.Ambient:
+                _ambientVolume = newVolume;
+                UpdateAmbientVolume();
+                return;
+        }
+        Debug.LogWarning("Unknown Volume Enum!");
+    }
+
+    public void PlayAtPosition(AudioClip clip, Vector3 position)
+    {
+        GameObject sourceObject = new GameObject("TempAudio");
+        sourceObject.transform.position = position;
+        AudioSource src = sourceObject.AddComponent<AudioSource>();
+        src.clip = clip;
+        src.volume = 
+        src.spatialBlend = 1f; // 3D
+        src.Play();
+        _sfxSources.Add(src);
+        Destroy(sourceObject, clip.length);
+    }
+
+    public AudioSource PlayLoopAtPosition(AudioClip clip, Vector3 position)
+    {
+        GameObject sourceObject = new GameObject("TempAudio");
+        sourceObject.transform.position = position;
+        AudioSource src = sourceObject.AddComponent<AudioSource>();
+        src.clip = clip;
+        src.volume =
+        src.spatialBlend = 1f; // 3D
+        src.Play();
+        _sfxSources.Add(src);
+        return src;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _musicSource = gameObject.AddComponent<AudioSource>();
+        _musicSource.clip = _mainMusic;
+        _musicSource.loop = true;
+        _musicSource.spatialBlend = 0f; // 2D
+        _musicSource.volume = _musicVolume;
+        _musicSource.Play();
+        Debug.Log("Playing music");
+
+        _sfxSources = new List<AudioSource>();
+
+        _ambientSource = gameObject.AddComponent<AudioSource>();
+        _ambientSource.volume = _ambientVolume;
+        _ambientSource.spatialBlend = 0f;
+    }
+}
