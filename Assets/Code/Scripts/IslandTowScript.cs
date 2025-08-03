8358545c5551f8aae0a08a021845f40fd680afcc
1Tymon1 <tymonbielski@gmail.com>
2025-08-03 02:00:57 +0200

    the island is towable

--- Assets/Code/Scripts/IslandTowScript.cs
+++ Assets/Code/Scripts/IslandTowScript.cs
@@ -4,23 +4,56 @@ using UnityEngine;
 
 public class IslandTowScript : MonoBehaviour
 {
+    bool isTowed = false; // Flag to check if the island is being towed
+    public GameObject chrisActivator; // Reference to the Chris activator GameObject
+    public int nodeNum = 100;
+    public float fadeTime = 2f; // Time to fade out the island
+    public float holdTime = 1f; // Time to hold the island before fading out
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.GetComponent<FishCatcher>() != null)
+        if (collision.GetComponent<FishCatcher>() != null && !isTowed)
         {
-            Debug.Log("Island collided with FishCatcher");
-            FishCatcher fishCatcher = collision.GetComponent<FishCatcher>();
-            transform.parent = fishCatcher.colliderByoy.transform; // Set the parent of the island to the last buoy
+            GetComponent<PolygonCollider2D>().enabled = false; // Disable the collider to prevent further collisions
 
-            GameObject buoy = fishCatcher.colliderByoy.GetComponent<NetExtension>().followedPoint;
+            isTowed = true; // Set the flag to true to prevent multiple triggers
+
+            Color color = collision.GetComponent<FishCatcher>().GetComponent<MeshRenderer>().material.color; // Get the color of the FishCatcher
+
+            NetExtension stuff = FindObjectOfType<BuoySpawner>().LastBuoy;
             while (true)
             {
-                buoy = buoy.GetComponent<NetExtension>().followedPoint; // Get the next buoy in the chain
-                if (buoy.GetComponent<NetExtension>().followedPoint == null || fishCatcher.colliderByoy.GetComponent<NetExtension>().followedPoint.CompareTag("Boat"))
+                NetExtension oldStuff = stuff;
+                stuff = stuff.followedPoint.GetComponent<NetExtension>();
+                Destroy(oldStuff.gameObject); // Destroy the buoy
+                if (stuff == null || stuff.gameObject == null || stuff.gameObject.CompareTag("Boat"))
                 {
-                    break; // Exit the loop if there is no followed point
+                    break; // Exit the loop if we reach the last buoy
                 }
             }
+
+            FindAnyObjectByType<BuoySpawner>().addBuoys(nodeNum, color); // Add new buoys with the specified color
+            NetExtension netExtension = FindAnyObjectByType<BuoySpawner>().LastBuoy; // Get the last buoy after adding new ones
+
+            while (true)
+            {
+                netExtension.transform.position = transform.position; // Set the position of the buoy to the island's position
+                netExtension = netExtension.followedPoint.GetComponent<NetExtension>(); // Traverse to the last buoy
+
+                if (netExtension == null || netExtension.gameObject == null || netExtension.gameObject.CompareTag("Boat"))
+                {
+                    break; // Exit the loop if we reach the last buoy
+                }
+            }
+            transform.SetParent(FindAnyObjectByType<BuoySpawner>().LastBuoy.gameObject.transform);
+            chrisActivator.SetActive(false); // Activate the Chris activator GameObject
+        }
+    }
+
+    private void Update()
+    {
+        if (isTowed && ((Vector2)GameObject.FindGameObjectWithTag("VillageCenter").transform.position - (Vector2)transform.position).magnitude < 40)
+        {
+
         }
     }
 }
