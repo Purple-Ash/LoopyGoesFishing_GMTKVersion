f8e1d1dc5e32cf0833db6eee6ea6aadf50674a12
skay44 <michal.1805@wp.pl>
2025-08-22 23:54:20 +0200

    fish goal proximity variable, shark script

---
+++ Assets/Code/Scripts/PhysicalFish/SharkScript.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharkScript : FishScript
+{
+    protected override void OnCollisionStay2D(Collision2D collision)
+    {
+    }
+}
