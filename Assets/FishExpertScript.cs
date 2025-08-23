d82f5951d7861163f7190e6664c0fcd65791ac49
1Tymon1 <tymonbielski@gmail.com>
2025-08-23 16:14:18 +0200

    thadeus prototype

--- Assets/FishExpertScript.cs
+++ Assets/FishExpertScript.cs
@@ -17,6 +17,10 @@ public class FishExpertScript : BaseNPCScript
     public GameObject equipmentView;
     public GameObject backPanel;
 
+    public List<NewFishData> fishData;
+    private List<bool> discoveredFish;
+    private List<bool> talkedFish;
+
     [Header("Sounds")]
     [SerializeField] protected AudioClip discoverSound;
     [SerializeField] float discoverMultiplier = 0.5f;
@@ -26,6 +30,16 @@ public class FishExpertScript : BaseNPCScript
     void Start()
     {
         //SpawnEntities();
+        fishData.Sort((f1, f2) => {
+            if (f1.price > f2.price) return 1;
+            if (f1.price == f2.price) return 0;
+            return -1;
+        });
+        foreach (NewFishData f in fishData)
+        {
+            discoveredFish.Add(false); // Initialize the discoveredFish list with false values
+            talkedFish.Add(false); // Initialize the talkedFish list with false values
+        }
         audioManager = GameObject.FindGameObjectWithTag("AudioManager").GetComponent<AudioManager>();
     }
 
@@ -53,17 +67,52 @@ public class FishExpertScript : BaseNPCScript
 
     void DestroyEntities()
     {
-        Debug.Log("Destroying Equipment entities");
         for (int i = 0; i < contentTransform.childCount; i++)
         {
-            Debug.Log("Destroying Equipment entities");
             Destroy(contentTransform.GetChild(i).gameObject);
         }
     }
 
     void SpawnEntities()
     {
+        Dictionary<NewFishData, int[]> fishDataDictionary = GameObject.FindGameObjectWithTag("EquipementManager").GetComponent<EquipementScript>().fishDataDictionary;
+        int caught = 0;
+        foreach (NewFishData f in fishData)
+        {
 
+            GameObject inst = Instantiate(entityToSpawn, contentTransform.transform);
+
+            Image[] fishImages = inst.GetComponentsInChildren<Image>();
+            TMP_Text[] textFields = inst.GetComponentsInChildren<TMP_Text>();
+            if (fishDataDictionary.ContainsKey(f))
+            {
+                fishImages[1].sprite = f.image;
+
+                textFields[0].SetText(f.name);
+                textFields[1].SetText(f.description);
+
+                caught++;
+
+                ColorUtility.TryParseHtmlString("#CFC2A1", out Color baseTextColor);
+
+                textFields[0].color = baseTextColor;
+                textFields[1].color = baseTextColor;
+
+            }
+            else
+            {
+                fishImages[1].sprite = f.image;
+                fishImages[1].color = Color.black;
+
+                textFields[0].SetText("???");
+                textFields[1].SetText("???");
+
+                textFields[0].color = Color.gray * 0.70f;
+                textFields[1].color = Color.gray * 0.70f;
+
+                fishImages[0].color = Color.gray;
+            }
+        }
     }
 
     public void UpdateValues()
